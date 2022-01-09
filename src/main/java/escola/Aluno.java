57418817387cafb62ad4411e41bece0ffca46363
Marcelo dos Santos <marcelo17082000@gmail.com>
2022-01-08 22:14:46 -0300

    Implementado DP builder e static method factory para simplificar a criação de alunos

--- src/main/java/escola/Aluno.java
+++ src/main/java/escola/Aluno.java
@@ -1,18 +1,31 @@
 package escola;
 
+import lombok.Builder;
 import lombok.Data;
-import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // É uma entidade pois tem um Identificador (o CPF)
 @Data
+@Builder
 public class Aluno {
 
     private CPF cpf;
     private String nome;
     private Email email;
     private List<Telefone> telefones;
+
+    private Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.telefones = new ArrayList<>();
+    }
+
+    public static Aluno comNomeCPFEmail(String nome, CPF cpf, Email email) {
+        return new Aluno(cpf, nome, email);
+    }
 
     public void adicionarTelefone(String ddd, String numero) {
         telefones.add(new Telefone(ddd, numero));
---
+++ src/main/java/escola/Indicacao.java
@@ -0,0 +1,14 @@
+package escola;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Indicacao {
+
+    private Aluno indicado;
+    private Aluno indicante;
+
+}
